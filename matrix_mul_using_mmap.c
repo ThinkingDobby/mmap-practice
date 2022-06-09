e7dd4827bfea23dc963bfce95b41a9d58fbf611e
ThinkingDobby <chiho1221@naver.com>
2022-06-09 23:13:26 +0900

    mmap 기본 동작 구현
    <FIN>
    파일 입력 받아 내용 출력하도록 구현
    - 파일 크기 fstat으로 획득
    <TODO>
    행렬곱 함수 구하기
    입력 타입 설정해 행렬 크기별 동작 구현

--- matrix_mul_using_mmap.c
+++ matrix_mul_using_mmap.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -7,45 +8,45 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char **argv)
-{
+#define BUF_SIZE 100
+
+void error_handling(char* message);
+
+int main(int argc, char** argv) {
     int fd;
     char *file = NULL;
+    int file_size;
+
     struct stat sb;
-    char buf[80] ={0x00,};
+    char buf[BUF_SIZE];
     int flag = PROT_WRITE | PROT_READ;
 
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: input\n");
-        exit(1);
-    }
+    if (argc < 2) 
+	    printf("Usage: %s <file name>\n", argv[1]);
 
     if ((fd = open(argv[1], O_RDWR|O_CREAT)) < 0)
-    {
-        perror("File Open Error");
-        exit(1);
-    }
+	    error_handling("file open error");
 
     if (fstat(fd, &sb) < 0)
-    {
-        perror("fstat error");
-        exit(1);
-    }
+	    error_handling("fstat error");
+    file_size = (int)sb.st_size;
 
-    file = (char *)malloc(40);
 
-    // mmap를 이용해서 열린 파일을 메모리에 대응시킨다.
-    // file은 대응된 주소를 가리키고, file을 이용해서 필요한 작업을
-    // 하면 된다.
-    if ((file =
-        (char *) mmap(0, 40, flag, MAP_SHARED, fd, 0)) == -1)
-    {
-        perror("mmap error");
-        exit(1);
-    }
-    printf("%s\n", file);
-    memset(file, 0x00, 40);
-    mnumap(file);
+    file = (char *)malloc(file_size);
+
+    if ((file = (char *)mmap(0, file_size, flag, MAP_SHARED, fd, 0)) == NULL)
+	    error_handling("mmap error");
+
+    printf("%s", file);
+
+    munmap(file, file_size);
     close(fd);
+
+    return 0;
+}
+
+void error_handling(char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
