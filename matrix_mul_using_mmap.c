6af1152296b8ad1b345b1085036e80c148a2f103
ThinkingDobby <chiho1221@naver.com>
2022-06-10 23:28:19 +0900

    문자열 - 정수 배열 변환

--- matrix_mul_using_mmap.c
+++ matrix_mul_using_mmap.c
@@ -10,6 +10,7 @@
 
 #define BUF_SIZE 100
 
+int** mem_to_arr(char* mem);
 void error_handling(char* message);
 
 int main(int argc, char** argv) {
@@ -20,6 +21,9 @@ int main(int argc, char** argv) {
     struct stat sb;
     char buf[BUF_SIZE];
     int flag = PROT_WRITE | PROT_READ;
+
+    int** matrix1;
+    int** matrix2;
 
     if (argc < 2) 
 	    printf("Usage: %s <file name>\n", argv[1]);
@@ -37,13 +41,63 @@ int main(int argc, char** argv) {
     if ((file = (char *)mmap(0, file_size, flag, MAP_SHARED, fd, 0)) == NULL)
 	    error_handling("mmap error");
 
-    printf("%s", file);
+    // 메모리에 있는 데이터를 행렬 형태로 변환
+    matrix1 = mem_to_arr(file);
 
     munmap(file, file_size);
     close(fd);
 
     return 0;
 }
+
+int** mem_to_arr(char* mem) {
+	int** matrix;
+	int row = 0, col = 0;
+
+	int i = 0;
+	
+	while (1) {
+		if (mem[i] == ' ')
+			col++;
+		if (mem[i] == '\n') {
+			col++;
+			break;
+		}
+		i++;
+	}
+
+	i = 0;
+	while (mem[i]) {
+		if (mem[i] == '\n')
+			row++;
+		i++;
+	}
+
+	i = 0;
+	int j = 0;
+	// 이 부분에서 배열을 동적할당 하도록 하고
+	int arr[4] = { 0, };
+
+	// 이 밖에 반복문 하나 더 둘러서 모든 줄 정수로 변환할 것
+	// 지금은 한 줄만 정수 변환
+	for (i = 0; mem[i] != '\n'; i++) {
+		if (mem[i] == ' ') 
+			j++;
+		else 
+			arr[j] = arr[j] * 10 + (mem[i] - 48);
+	}
+
+	i = 0;
+	for (i = 0; i < col; i++)
+		printf("%d\n", arr[i]);
+
+	// 이런 식으로 두 개 행렬을 모두 이차원 배열에 저장하면, 각 배열의 행, 열수 바탕으로
+	// 행렬 곱 계산하는 함수 만들어 구현
+	// 이후 실행 결과 확인
+
+	return matrix;
+}
+
 
 void error_handling(char* message) {
 	fputs(message, stderr);
